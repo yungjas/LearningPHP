5158b9664558b93aea0119b4537b7b2c6ec80a01
jas <jasmine-lim88@hotmail.com>
2019-12-14 17:10:13 +0800

    First commit

---
+++ Intro/site.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Learning PHP</title>
+    </head>
+    <body>
+        <?php
+            echo("Hello World");
+        ?>
+    </body>
+</html>
