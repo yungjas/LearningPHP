d2d15f580acfe1cf8f332c0940d5573a15f15b17
jas <jasmine-lim88@hotmail.com>
2019-12-16 16:55:16 +0800

    Completed variables tutorial

---
+++ Variables/site.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Writing HTML</title>
+    </head>
+    <body>
+        <!--Variables are indicated by a $ sign/> -->
+        <?php
+            $characterName = "Tom";
+            $characterAge = 35;
+            echo "There once was a man named $characterName <br>";
+            echo "He was $characterAge years old <br>";
+            echo "He really liked the name $characterName <br>";
+            echo "But he didn't like being $characterAge";
+        ?>
+    </body>
+</html>
